82b1f326b87bb71d0c3e4dd17d7e06a4beb7b0e9
Sayan <sb04052002@gmail.com>
2023-04-19 13:00:56 +0530

    add all

---
+++ members/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  phone = models.IntegerField(null= True)
+# Create your models here.
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
---
+++ members/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+#def members(request):
+ #   template = loader.get_template('first.html')
+  #  return HttpResponse(template.render())
+# Create your views here.
+
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+    
+def testing(request):
+  mydata = Member.objects.order_by('firstname').all().values()
+  template = loader.get_template('testing.html')
+  context = {
+    'mymembers': mydata,
+  }
+  return HttpResponse(template.render(context, request))
+    
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember' : mymember,
+    }
+    return HttpResponse(template.render(context, request))
